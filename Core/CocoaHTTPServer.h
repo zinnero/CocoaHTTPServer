bc964c3f2aaefb6565aef54ff93431839b29e1d9
Christian Beer <christian.beer@chbeer.de>
2011-12-12 17:22:40 +0100

    Added global .h file

---
+++ Core/CocoaHTTPServer.h
@@ -0,0 +1,22 @@
+//
+//  CocoaHTTPServer.h
+//  CocoaHTTPServer
+//
+//  Created by Christian Beer on 12.12.11.
+//  Copyright (c) 2011 Christian Beer. All rights reserved.
+//
+
+#ifndef CocoaHTTPServer_CocoaHTTPServer_h
+#define CocoaHTTPServer_CocoaHTTPServer_h
+
+#import "HTTPServer.h"
+#import "HTTPConnection.h"
+#import "HTTPResponse.h"
+#import "HTTPMessage.h"
+
+#import "HTTPAsyncFileResponse.h"
+#import "HTTPDataResponse.h"
+#import "HTTPFileResponse.h"
+#import "HTTPDynamicFileResponse.h"
+
+#endif
